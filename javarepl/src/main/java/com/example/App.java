efe00f9cbb97b2b6d7bf17ac3562f03fe94e0f38
03milosevicN <nikolamilosevic220803@gmail.com>
2024-08-08 20:13:17 +0200

    Refactored App.java

--- javarepl/src/main/java/com/example/App.java
+++ javarepl/src/main/java/com/example/App.java
@@ -1,9 +1,48 @@
 package com.example;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.print("$ ");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String[] availableCommands = {"echo", "exit", "typeof"};
+        String substring;
+
+        //? main loop:
+        while (!input.isEmpty()) {
+
+            //? "exit" || "exit 0": exits shell
+            if (input.equals("exit 0") || input.equals("exit")) {
+                break;
+            }
+
+
+            //? "echo": print statement
+            if (input.startsWith("echo")) {
+                System.out.println(input.substring(5).trim());
+            }
+
+
+            //? "typeof": is command valid
+            if (input.startsWith("typeof ")) {
+                substring = input.substring(7).trim(); 
+
+                if (Arrays.asList(availableCommands).contains(substring)) {
+                    System.out.println(substring + ": is a shell builtin");
+                } else {
+                    System.out.println(substring + ": not found");
+                }
+            }
+
+            System.out.print("$ ");
+            input = scanner.nextLine(); 
+        }
+
+        scanner.close();
     }
 }
